69bbe5ad9b0ea7e002ce422a0c7541f7891c8468
miiica-exe <xanje.archivum@gmail.com>
2026-06-11 09:19:05 -0600

    Create items.js

---
+++ items.js
@@ -0,0 +1,42 @@
+//==================================================
+// ITEMS
+//==================================================
+
+export const items = {
+
+    smallBone:{
+        id:"smallBone",
+
+        name:"Small Bone",
+
+        weight:1
+    },
+
+    bigBone:{
+        id:"bigBone",
+
+        name:"Big Bone",
+
+        weight:3
+    },
+
+    spoon:{
+        id:"spoon",
+
+        name:"Spoon",
+
+        weight:1,
+
+        special:true
+    },
+
+    scrapBook:{
+        id:"scrapBook",
+
+        name:"Scrapbook",
+
+        weight:2,
+
+        special:true
+    }
+};
